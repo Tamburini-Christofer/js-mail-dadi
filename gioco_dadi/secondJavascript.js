68404766873191929ff4759c373890829054d8da
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-01 15:27:14 +0200

    Esercizio 2 completato

--- gioco_dadi/secondJavascript.js
+++ gioco_dadi/secondJavascript.js
@@ -11,12 +11,12 @@ console.log(`Anche il pc ha lanciato il dado e ha ottenuto: ${tiroPc}`);
 //Vediamo chi vince
 if (tiroGiocatore > tiroPc){
     console.log("---Il vincitore è il giocatore!!!---")
-    console.log("Ti ho battuto, cassonetto: Il giocatore ha risposto!!! ")
+    console.log('"Ti ho battuto, cassonetto" Il giocatore ha risposto!!!')
 } else if (tiroGiocatore < tiroPc) {
     console.log("---Il vincitore è la macchina!!!---")
-    console.log("01101000 01101111 00100000 01110110 01101001 01101110 01110100 01101111: Il pc ha risposto!!!")
+    console.log('"01101000 01101111 00100000 01110110 01101001 01101110 01110100 01101111" Il pc ha risposto!!!')
 
 //In caso di pareggio
 } else {
-    console.log('I due sfidanti si guardano con odio! Pareggio!!! "Accidenti" disse il giocatore, "01100011 01100001 01111010 01111010 01101111" disse la macchina ')
+    console.log('I due sfidanti si guardano con odio! Pareggio!!! "Accidenti" disse il giocatore, "01100011 01100001 01111010 01111010 01101111" disse la macchina')
 }
