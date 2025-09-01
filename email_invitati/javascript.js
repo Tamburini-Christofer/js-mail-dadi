b68d0db4c58d63fa7b77e63930cdd62c41c821fa
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-01 13:53:55 +0200

    Primo esercizio completato

--- email_invitati/javascript.js
+++ email_invitati/javascript.js
@@ -26,3 +26,34 @@ const emailRegistrate = [
 //Chiediamo all'utente la propria Email
 const emailUtente = prompt("Inserisci il tuo indirizzo email:");
 
+//Creo una variabile Boolean
+let personaLista = false;
+
+//Controlliamo se l'utente è nella lista per la festa
+for(let i = 0; i < emailRegistrate.length; i++) {
+
+//Se l'indice dell'utente è uguale all'input dell'utente
+    if (emailRegistrate[i] === emailUtente) {
+
+//Verifica se è stato trovato la persona tra la lista
+    personaLista = true;
+
+//Ferma il ciclo for
+    break;
+    }
+}
+
+//Se la persona è nella lista, ovvero il valore Boolean è true
+if (personaLista) {
+
+//Stampami il messaggio di benvenuto
+    console.log ("Hey bello! Puoi entrare perchè sei nella lista. Buona festa.")
+}
+//Se la persona è nella lista, ovvero il valore Boolean è false
+else {
+
+//Stampami il messaggio di negazione
+    console.log ("Mi dispiace, ma qui non ci sei scritto, bro!")
+}
+
+
