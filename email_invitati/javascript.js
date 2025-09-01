146c9a404ca576abc856d7241b745554d968991a
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-01 13:21:48 +0200

    first commit

---
+++ email_invitati/javascript.js
@@ -0,0 +1,28 @@
+//Creiamo un array contenente tutte l'email registrate
+
+const emailRegistrate = [
+  "mario.rossi23@example.com", 
+  "anna.bianchi87@example.com",
+  "giovanni.verdi12@example.com",
+  "laura.ferrari99@example.com",
+  "federico.conti45@example.com",
+  "elena.galli78@example.com",
+  "andrea.morini56@example.com",
+  "francesca.lombardi34@example.com",
+  "alessandro.pace67@example.com",
+  "martina.moretti21@example.com",
+  "simone.rinaldi88@example.com",
+  "valentina.marini11@example.com",
+  "luca.costa90@example.com",
+  "giulia.martinelli53@example.com",
+  "daniele.santini76@example.com",
+  "chiara.deleo44@example.com",
+  "riccardo.fabbri32@example.com",
+  "ilaria.brunetti25@example.com",
+  "stefano.giorgi60@example.com",
+  "francesca.romani15@example.com"
+];
+
+//Chiediamo all'utente la propria Email
+const emailUtente = prompt("Inserisci il tuo indirizzo email:");
+
